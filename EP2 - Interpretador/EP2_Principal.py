6c02797f563e4b4e6f24c00ab59f1bd67c8a9f20
gugres <gustavo.giro.resende@gmail.com>
2019-10-05 12:40:04 -0300

    Interpreta expressoes e converte para uma lista em notacao normal

--- EP2 - Interpretador/EP2_Principal.py
+++ EP2 - Interpretador/EP2_Principal.py
@@ -1,3 +1,5 @@
+'''Desafio - Construir um Shell Mode do Python, utilizando um ADT de uma pilha e notação polonesa'''
+
 # As expressões podem conter:
 # a)Constantes inteiras (ex: 132, -45, 123456789, etc.)
 # b)Variáveis contendo apenas letras (ex: k, SoMa, contador, QuantaDor, etc.)
@@ -7,98 +9,126 @@
 # f)Atribuição: =
 # Subistituir "-" por "_" e "+" por "#" quando forem unários
 
+class Empty (Exception):
+  pass
+
 class _Node:
-    '''classe _Node - interna'''
+  '''classe _Node - interna'''
 
-    __slots__ = '_info', '_prox'
+  __slots__ = '_info', '_prox'
 
-    def __init__ (self, info, prox):
-        # inicia os campos
-        self._info = info
-        self._prox = prox
-
+  def __init__(self, info, prox):
+    # inicia os campos
+    self._info = info
+    self._prox = prox
 
 class Pilha:
-    ''' implementa uma pilha usando uma Lista Ligada simples. '''
+  ''' implementa uma pilha usando uma Lista Ligada simples. '''
 
-    def __init__ (self):
-        ''' cria uma pilha vazia.'''
-        self._topo = None # vazia
-        self._tamanho = 0 # tamanho da pilha
+  __slots__ = '_topo', '_tamanho'
 
-    def __len__(self):
-        return self._tamanho
+  def __init__(self):
+    ''' cria uma pilha vazia.'''
+    self._topo = None  # vazia
+    self._tamanho = 0  # tamanho da pilha
 
-    def is_empty(self):
-        ''' retorna True se pilha vazia'''
-        return self._tamanho == 0
+  def __len__(self):
+    return self._tamanho
 
-    def push(self, e):
-        ''' adiciona elemento ao topo da pilha.'''
-        novo = _Node(e, self._topo)
-        self._topo = novo
-        self._tamanho += 1
+  def is_empty(self):
+    ''' retorna True se pilha vazia'''
+    return self._tamanho == 0
 
-    def top(self):
-        ''' retorna sem remover o topo da pilha.
-        sinaliza exceção se pilha vazia.'''
-        if self.is_empty():
-            raise Empty("Pilha Vazia")
-        return self._topo._info # topo da pilha
+  def push(self, e):
+    ''' adiciona elemento ao topo da pilha.'''
+    novo = _Node(e, self._topo)
+    self._topo = novo
+    self._tamanho += 1
 
-    def pop(self):
-        ''' remove e retorna o topo da pilha.
-        sinaliza exceção se pilha vazia.'''
-        if self.is_empty():
-            raise Empty("Pilha Vazia")
-        val_topo = self._topo._info
-        self._topo = self._topo._prox # pula o primeiro elemento
-        self._tamanho -= 1
-        return val_topo
+  def top(self):
+    ''' retorna sem remover o topo da pilha.
+    sinaliza exceção se pilha vazia.'''
+    if self.is_empty():
+        raise Empty("Pilha Vazia")
+    return self._topo._info  # topo da pilha
 
-    def Imprime(self):
-        p = self._topo
-        print("Imprimindo a pilha")
-        while p is not None:
-            print(p._info)
-            p = p._prox
-            
-def main ():
+  def pop(self):
+    ''' remove e retorna o topo da pilha.
+    sinaliza exceção se pilha vazia.'''
+    if self.is_empty():
+        raise Empty("Pilha Vazia")
+    val_topo = self._topo._info
+    self._topo = self._topo._prox  # pula o primeiro elemento
+    self._tamanho -= 1
+    return val_topo
 
-    while True:
-        #Colocar o prompt
-        #Solicita uma nova expressão do tipo:
-        expressao = str(input(">>> "))
-        novaPilha = Pilha()
-        if Operadores(expressao) != None:
-            novaPilha.push(expressao)
-            continue
-        print (novaPilha.Imprime())
-        return
-        #Traduzir a notação para pós-fixas
+  def Imprime(self):
+    p = self._topo
+    print("Imprimindo a pilha")
+    while p is not None:
+        print(p._info)
+        p = p._prox
 
-        # Calcular o valor da expressão usando a notação pós-fixas
-        # Se o final for "=" atribuição, armazenar o valor. Caso contrario, print
+def main():
+  '''Função principal'''
+
+  while True:
+    # Colocar o prompt
+    # Solicita uma nova expressão do tipo:
+    expressao = str(input(">>> "))
+    novaPilha = Pilha()
+    if expressao == "0": return
+    print (expressao)
+
+    # Cria uma nova param, separando operadores de operandos
+    novaExpressao = NovaExpressao(expressao)
+    # Transforma em uma lista
+    novaExpressao = novaExpressao.split()
+    print (novaExpressao)
+
+    # Traduzir a notação para pós-fixas
+
+    # Calcular o valor da expressão usando a notação pós-fixas
+    # Se o final for "=" atribuição, armazenar o valor. Caso contrario, print
 
 def Operadores(param):
-    '''Verifica qual o operador'''
-    operadores = ["+","-","*","/","**","(",")","="]
-    if param in operadores:
-        for i in range (len(operadores)):
-            if param == operadores[i]: return i
-    return None
-    
-def TraduzPosFixa():
-    '''Recebe uma string "expressao" com ou sem atribuição "="
-    e devolve uma lista contendo essa expressão em notação pós-fixas.
-    Devolve True se foi realizado com sucesso, ou False caso contrário'''
-    pass
+  '''Verifica qual o operador'''
+  operadores = ["=", "+", "-", "*", "/", "**", "(", ")"]
+  if param in operadores:
+      for i in range(len(operadores)):
+          if param == operadores[i]:
+              return i
+  return None
+
+def NovaExpressao(param):
+  '''Separa os operandos dos operadores na string, independentemente de terem
+  espaços entre eles ou não'''
+  novaExpressao = ""
+  for i in range (len(param)):
+    if param[i] == "*" and param[i+1] == "*":
+      novaExpressao = novaExpressao + " " + param[i]
+      continue
+    if param[i] == "*" and param[i-1] == "*":
+      novaExpressao = novaExpressao + param[i] + " "
+      continue
+    elif Operadores(param[i]) != None:
+      novaExpressao = novaExpressao + " " + param[i] + " "
+      continue
+    else:
+      novaExpressao += param[i]
+      continue
+  return novaExpressao
+
+def TraduzPosFixa(param):
+  '''Recebe uma string "param" com ou sem atribuição "="
+  e devolve uma lista contendo essa expressão em notação pós-fixas.
+  Devolve True se foi realizado com sucesso, ou False caso contrário'''
+  pass
 
 def CalcPosFixa():
-    '''Recebe uma lista de valores contendo uma expressão em notação pós-fixa e
-    calcula o seu valor. Devolve esse valor se o calculo foi feito com sucesso,
-    ou False caso contrário'''
-    pass
+  '''Recebe uma lista de valores contendo uma expressão em notação pós-fixa e
+  calcula o seu valor. Devolve esse valor se o calculo foi feito com sucesso,
+  ou False caso contrário'''
+  pass
 
 main()
-
